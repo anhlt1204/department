2d6def2e322d5564a0fea327512ab6888ab9f939
DESKTOP-88FAOV3\EdsoLabs <>
2021-11-25 08:58:00 +0700

    deploy

---
+++ src/main/java/com/leanh/departmentservice/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.leanh.departmentservice.repository;
+
+import com.leanh.departmentservice.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Department findByDepartmentId(Long departmentId);
+}
